8a3d08cedf1b02680053178197dc1e5c456ae479
Ekaterina Hlebalova <khlebalova.e@yandex.ru>
2020-06-09 00:51:25 +0400

    исходники проекта добавлены

---
+++ src/Deposit.java
@@ -0,0 +1,8 @@
+public class Deposit {
+    public static void main(String[] args) {
+        int deposit = 2_000_000_000;
+        int transfer = 500_000_000;
+        int total = deposit + transfer;
+        System.out.println(total);
+    }
+}
